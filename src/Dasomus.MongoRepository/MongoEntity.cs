a06cf579d134bafd4b59d16b7d9f01d4ac965a6c
Henry Oh <hodude_99@yahoo.com>
2017-04-26 06:43:47 -0700

    add entity class

---
+++ src/Dasomus.MongoRepository/MongoEntity.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dasomus.MongoRepository
+{
+    /// <summary>
+    /// Abstract Entity for all the MongoEntities.
+    /// </summary>
+    [BsonIgnoreExtraElements(Inherited = true)]
+    public abstract class MongoEntity<TKey> : IMongoEntity<TKey>
+    {
+        /// <summary>
+        /// Gets or sets the id for this object (the primary record for an entity).
+        /// </summary>
+        /// <value>The id for this object (the primary record for an entity).</value>
+        [BsonRepresentation(BsonType.ObjectId)]
+        public TKey Id { get; set; }
+    }
+}
