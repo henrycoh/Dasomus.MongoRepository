5fb8653888adf5d09d55c9722428604e606cad04
Henry Oh <hodude_99@yahoo.com>
2017-04-28 06:46:04 -0700

    added default abstract entity class

--- src/Dasomus.MongoRepository/MongoEntity.cs
+++ src/Dasomus.MongoRepository/MongoEntity.cs
@@ -19,4 +19,18 @@ namespace Dasomus.MongoRepository
         [BsonRepresentation(BsonType.ObjectId)]
         public TKey Id { get; set; }
     }
+
+    /// <summary>
+    /// Default Abstract Entity for all the MongoEntities.
+    /// </summary>
+    [BsonIgnoreExtraElements(Inherited = true)]
+    public abstract class MongoEntity : IMongoEntity<ObjectId>
+    {
+        /// <summary>
+        /// Gets or sets the id for this object (the primary record for an entity).
+        /// </summary>
+        /// <value>The id for this object (the primary record for an entity).</value>
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+    }
 }
