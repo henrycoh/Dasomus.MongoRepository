96e6ad51a2748f237d60d88bf84af7c604c12ed7
Henry Oh <hodude_99@yahoo.com>
2017-05-19 06:29:37 -0700

    resolved build issue

---
+++ src/Dasomus.MongoRepository/Attributes/CollectionName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dasomus.MongoRepository
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public class CollectionName : Attribute
+    {
+        public CollectionName(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+                throw new Exception("Name cannot be null or empty string");
+
+            this.Name = name;
+        }
+        
+        public string Name { get; private set; }
+    }
+}
--- src/Dasomus.MongoRepository/Util.cs
+++ src/Dasomus.MongoRepository/Util.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
+using ReflectionBridge.Extensions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Dasomus.MongoRepository
 {
@@ -11,51 +13,18 @@ namespace Dasomus.MongoRepository
     /// </summary>
     internal static class Util<U>
     {
-        /// <summary>
-        /// The default key MongoRepository will look for in the App.config or Web.config file.
-        /// </summary>
-        private const string DefaultConnectionstringName = "MongoServerSettings";
-
-        /// <summary>
-        /// Retrieves the default connectionstring from the App.config or Web.config file.
-        /// </summary>
-        /// <returns>Returns the default connectionstring from the App.config or Web.config file.</returns>
-        public static string GetDefaultConnectionString()
-        {
-            return ConfigurationManager.ConnectionStrings[DefaultConnectionstringName].ConnectionString;
-        }
-
-        /// <summary>
-        /// Creates and returns a MongoDatabase from the specified url.
-        /// </summary>
-        /// <param name="url">The url to use to get the database from.</param>
-        /// <returns>Returns a MongoDatabase from the specified url.</returns>
         private static IMongoDatabase GetDatabaseFromUrl(MongoUrl url)
         {
             var client = new MongoClient(url);
-            var server = client.GetServer();
-            return server.GetDatabase(url.DatabaseName); // WriteConcern defaulted to Acknowledged
+            return client.GetDatabase(url.DatabaseName);
         }
 
-        /// <summary>
-        /// Creates and returns a MongoCollection from the specified type and connectionstring.
-        /// </summary>
-        /// <typeparam name="T">The type to get the collection of.</typeparam>
-        /// <param name="connectionString">The connectionstring to use to get the collection from.</param>
-        /// <returns>Returns a MongoCollection from the specified type and connectionstring.</returns>
         public static IMongoCollection<T> GetCollectionFromConnectionString<T>(string connectionString)
             where T : IMongoEntity<U>
         {
             return Util<U>.GetCollectionFromConnectionString<T>(connectionString, GetCollectionName<T>());
         }
 
-        /// <summary>
-        /// Creates and returns a MongoCollection from the specified type and connectionstring.
-        /// </summary>
-        /// <typeparam name="T">The type to get the collection of.</typeparam>
-        /// <param name="connectionString">The connectionstring to use to get the collection from.</param>
-        /// <param name="collectionName">The name of the collection to use.</param>
-        /// <returns>Returns a MongoCollection from the specified type and connectionstring.</returns>
         public static IMongoCollection<T> GetCollectionFromConnectionString<T>(string connectionString, string collectionName)
             where T : IMongoEntity<U>
         {
@@ -63,25 +32,12 @@ namespace Dasomus.MongoRepository
                 .GetCollection<T>(collectionName);
         }
 
-        /// <summary>
-        /// Creates and returns a MongoCollection from the specified type and url.
-        /// </summary>
-        /// <typeparam name="T">The type to get the collection of.</typeparam>
-        /// <param name="url">The url to use to get the collection from.</param>
-        /// <returns>Returns a MongoCollection from the specified type and url.</returns>
         public static IMongoCollection<T> GetCollectionFromUrl<T>(MongoUrl url)
             where T : IMongoEntity<U>
         {
             return Util<U>.GetCollectionFromUrl<T>(url, GetCollectionName<T>());
         }
 
-        /// <summary>
-        /// Creates and returns a MongoCollection from the specified type and url.
-        /// </summary>
-        /// <typeparam name="T">The type to get the collection of.</typeparam>
-        /// <param name="url">The url to use to get the collection from.</param>
-        /// <param name="collectionName">The name of the collection to use.</param>
-        /// <returns>Returns a MongoCollection from the specified type and url.</returns>
         public static IMongoCollection<T> GetCollectionFromUrl<T>(MongoUrl url, string collectionName)
             where T : IMongoEntity<U>
         {
@@ -89,15 +45,10 @@ namespace Dasomus.MongoRepository
                 .GetCollection<T>(collectionName);
         }
 
-        /// <summary>
-        /// Determines the collectionname for T and assures it is not empty
-        /// </summary>
-        /// <typeparam name="T">The type to determine the collectionname for.</typeparam>
-        /// <returns>Returns the collectionname for T.</returns>
         private static string GetCollectionName<T>() where T : IMongoEntity<U>
         {
             string collectionName;
-            if (typeof(T).BaseType.Equals(typeof(object)))
+            if (typeof(T).BaseType().Equals(typeof(object)))
             {
                 collectionName = GetCollectioNameFromInterface<T>();
             }
@@ -113,20 +64,13 @@ namespace Dasomus.MongoRepository
             return collectionName;
         }
 
-        /// <summary>
-        /// Determines the collectionname from the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type to get the collectionname from.</typeparam>
-        /// <returns>Returns the collectionname from the specified type.</returns>
         private static string GetCollectioNameFromInterface<T>()
         {
             string collectionname;
-
-            // Check to see if the object (inherited from Entity) has a CollectionName attribute
-            var att = Attribute.GetCustomAttribute(typeof(T), typeof(CollectionName));
+            
+            var att = typeof(T).GetTypeInfo().GetCustomAttribute(typeof(CollectionName));
             if (att != null)
             {
-                // It does! Return the value specified by the CollectionName attribute
                 collectionname = ((CollectionName)att).Name;
             }
             else
@@ -137,30 +81,22 @@ namespace Dasomus.MongoRepository
             return collectionname;
         }
 
-        /// <summary>
-        /// Determines the collectionname from the specified type.
-        /// </summary>
-        /// <param name="entitytype">The type of the entity to get the collectionname from.</param>
-        /// <returns>Returns the collectionname from the specified type.</returns>
         private static string GetCollectionNameFromType(Type entitytype)
         {
             string collectionname;
 
-            // Check to see if the object (inherited from Entity) has a CollectionName attribute
-            var att = Attribute.GetCustomAttribute(entitytype, typeof(CollectionName));
+            var att = entitytype.GetTypeInfo().GetCustomAttribute(typeof(CollectionName));
             if (att != null)
             {
-                // It does! Return the value specified by the CollectionName attribute
                 collectionname = ((CollectionName)att).Name;
             }
             else
             {
                 if (typeof(MongoEntity).IsAssignableFrom(entitytype))
                 {
-                    // No attribute found, get the basetype
-                    while (!entitytype.BaseType.Equals(typeof(MongoEntity)))
+                    while (!entitytype.BaseType().Equals(typeof(MongoEntity)))
                     {
-                        entitytype = entitytype.BaseType;
+                        entitytype = entitytype.BaseType();
                     }
                 }
                 collectionname = entitytype.Name;
