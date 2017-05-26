a240eaf89692d6b24bf9ddd63dd250310745503d
Henry Oh <hodude_99@yahoo.com>
2017-05-26 06:39:25 -0700

    added test

---
+++ src/Dasomus.MongoRepository.Tests/CarRepositoryTest.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dasomus.MongoRepository.Tests
+{
+    [CollectionName("car_collection")]
+    [BsonIgnoreExtraElements]
+    public class Car : MongoEntity
+    {
+        [BsonElement("year")]
+        public int Year { get; set; }
+        [BsonElement("make")]
+        public string Make { get; set; }
+        [BsonElement("model")]
+        public string Model { get; set; }
+    }
+
+    public class CarRepositoryTest
+    {
+        public void AddCarTest()
+        {
+            var context = new MongoContext("mongodb://localhost/CarDatabase");
+            var repo = new MongoRepository<Car>(context);
+
+            var newCar = new Car
+            {
+                Year = 1999,
+                Make = "Toyota",
+                Model = "Camry"
+            };
+
+            repo.Add(newCar);
+        }
+    }
+}
